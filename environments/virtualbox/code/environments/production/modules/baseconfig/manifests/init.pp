e8fd2736b57a92a5b8a22b5bd04f9a76ed983ee8
matheus_dmp19 <matheus_dmp19@hotmail.com>
2023-09-19 09:20:38 -0300

    update puppet files vb env

--- environments/virtualbox/code/environments/production/modules/baseconfig/manifests/init.pp
+++ environments/virtualbox/code/environments/production/modules/baseconfig/manifests/init.pp
@@ -1,10 +1,19 @@
 # Class baseconfig
 class baseconfig (
   $user_matheus = $baseconfig::params::user_matheus,
+  $linux_users = $baseconfig::params::linux_users
 ) inherits baseconfig::params{
-  user { 'matheus':
-      ensure     => $user_matheus,
-      managehome => 'true',
-      home       => '/home/matheus'
+    user { 'matheus':
+        ensure     => $user_matheus,
+        managehome => 'true',
+        home       => '/home/matheus'
+    }
+
+    $linux_users.each |$user| {
+      user { $user:
+        ensure     => 'present',
+        managehome => 'true',
+        home       => "/home/${user}"
+      }
+    }
   }
-}
--- environments/virtualbox/code/environments/production/modules/baseconfig/manifests/params.pp
+++ environments/virtualbox/code/environments/production/modules/baseconfig/manifests/params.pp
@@ -1,4 +1,5 @@
 # Params 
 class baseconfig::params {
   $user_matheus = hiera('baseconfig::params::user_matheus', 'present')
+  $linux_users = hiera('baseconfig::params::linux_users')
 }
